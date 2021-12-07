277cbc1ae91a376226fadf83d237011577e40265
Adib Gonçalves Fernandes de Souza <adib.fernandes@gmail.com>
2021-12-07 09:30:20 -0300

    Ajustes em teste com abstracoes erradas

--- cypress/integration/wrong-abstraction/sample.spec.js
+++ cypress/integration/wrong-abstraction/sample.spec.js
@@ -9,24 +9,22 @@ describe('Wrong abstraction bad practice', () => {
     cy.wait('@getStories')
   })
 
+  afterEach(() => {
+    cy.get('.table-row').its('length').should('be.at.least', 1)
+  })
+
   it('uses custom command for assertion just for the sake of reusability', () => {
     cy.search('cypress')
     cy.wait('@getStories')
-
-    cy.assertResults()
   })
 
   it('uses custom command for assertion just for the sake of reusability', () => {
     cy.search('selenium')
     cy.wait('@getStories')
-
-    cy.assertResults()
   })
 
   it('uses custom command for assertion just for the sake of reusability', () => {
     cy.search('playwright')
     cy.wait('@getStories')
-
-    cy.assertResults()
   })
 })
